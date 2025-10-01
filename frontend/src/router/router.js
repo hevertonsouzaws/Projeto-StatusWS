f8d310099282449d88511eb95db1f082920e37d5
Heverton WS <heverton.souza@websupply.com.br>
2025-10-01 14:46:31 -0300

    fix: Implementando createWebHashHistory

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { usuarioLogado, usuarioLogadoId, isAdmin } from '../modeState.js';
 import Login from '../views/Login.vue'; 
 import HomeView from '../views/HomeView.vue';
@@ -15,7 +15,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 const isUserAdmin = isAdmin.value;
